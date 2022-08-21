9dfbab082a4c09b10a522616a33ca414dce14ed4
Yuriy Gudimov <fash1k@gmail.com>
2022-08-21 14:28:15 +0300

    Initial Commit

---
+++ Snakev2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snakev2
+//
+//  Created by advanc3d on 21.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
